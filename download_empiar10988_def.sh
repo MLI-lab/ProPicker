a47cfd245cbc5c3b8886409312c1acded27a5df9
SimWdm <wiedemann.simon@yahoo.de>
2025-03-04 09:36:43 +0000

    remove download shell scripts

--- download_empiar10988_def.sh
+++
@@ -1,9 +0,0 @@
-# make base directory for empiar datasets
-mkdir -p data/empiar
-cd data/empiar
-# download particle coordinages
-wget -r -np -nH --cut-dirs=2 -R "index.html*" -e robots=off https://ftp.ebi.ac.uk/empiar/world_availability/10988/data/DEF/particle_lists/
-# download tomograms
-wget -r -np -nH --cut-dirs=2 -R "index.html*" -e robots=off https://ftp.ebi.ac.uk/empiar/world_availability/10988/data/DEF/tomograms/
-# download ground truth annotations
-wget -r -np -nH --cut-dirs=2 -R "index.html*" -e robots=off https://ftp.ebi.ac.uk/empiar/world_availability/10988/data/DEF/labels/
